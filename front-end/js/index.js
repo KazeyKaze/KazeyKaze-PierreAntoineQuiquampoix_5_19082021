dd0922c115ef6cca185ec6ef390c89e1b706c251
Pierre-Antoine Quiquampoix <p.a.q@hotmail.fr>
2021-08-26 18:20:52 +0200

    Avancements page d'acceuil

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,7 +1,27 @@
 const API = function () {
     fetch("http://localhost:3000/api/furniture")
-        .then(response => {
+        .then((response) => {
             let data = response.json();
+            return data;
+        })
+        .then(function (data) {
+            let displayImg = document.querySelector(".produits1__img");
+            displayImg.src = data[0].imageUrl;
+
+            let displayDescription = document.querySelector(".produits1__description");
+            displayDescription.innerHTML = data[0].description;
+
+            let displayName = document.querySelector(".produits1__name");
+            displayName.innerHTML = `- ${data[0].name} -`;
+
+            data[0].price = data[0].price / 100;
+            let displayPrice = document.querySelector(".produits1__price");
+            displayPrice.innerHTML = data[0].price.toFixed(2) + " €";
+
+            let varnish = [...data[0].varnish]
+            let displayVarnish = document.querySelector(".produits1__varnish");
+            displayVarnish.innerHTML = varnish.join(" / ");
+
             console.log(data);
         })
         .catch(error => alert("Erreur : " + error));

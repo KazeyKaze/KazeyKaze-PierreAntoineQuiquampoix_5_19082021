fae026a1524a1fe96c924f02051bc2fc2e6dda14
Pierre-Antoine Quiquampoix <p.a.q@hotmail.fr>
2021-09-30 11:28:19 +0200

    Réécriture des commentaires

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,17 +1,32 @@
-// Fetch des données produits
+// Fonction qui fetch les données des produits
 function getProducts() {
+
+    // Précision de l'URL à fetch
     return fetch("http://localhost:3000/api/furniture")
+
+        // Si il y a une réponse, je transforme la réponse en format JSON
         .then((response) => {
             return response.json();
         })
+
+        // Si il n'y a pas de réponse, je renvoi un message d'erreur grâce au catch
         .catch(error => alert("Erreur : " + error));
 }
 
-// Création du HTML pour un produit
+
+
+// Je cible un id HTML pour injecter du contenu à l'intérieur
 let plan = document.getElementById("produits");
 
+
+
+// Fonction qui contient le code HTML et les données que je veux injecter
 function getProductTemplate(product) {
+
+    // Variable qui me sert à concaténer les vernis afin de les afficher
     let varnish = [...product.varnish];
+
+    // Plan HTML et données qui seront injectées
     return `<a href="/front-end/html/page_produit.html?id=${product._id}">
             <div class="produits" id="produits__">
             <img class="produits__img" src="${product.imageUrl}" alt="Image de ${product.name} ">
@@ -23,10 +38,16 @@ function getProductTemplate(product) {
             </a>`
 }
 
-// Itération du HTML pr chaque produit
+
+
+// Fonction qui permet d'appliquer le plan HTML pour chaque produit
 getProducts()
+
+    // Pour chaque produit contenu dans les produits
     .then(products => {
         for (const product of products) {
+
+            // J'applique le plan HTML
             plan.innerHTML += getProductTemplate(product);
         }
     })
